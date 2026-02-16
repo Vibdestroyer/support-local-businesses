3acfd6a0a9ee7b8076af22b87b221a53ce234360
Victor Braday <Vibrad77@gmail.com>
2026-02-15 22:11:36 -0500

    testing business page

---
+++ app/business/page.tsx
@@ -0,0 +1,8 @@
+export default function BusinessPage() {
+  return (
+    <div className="min-h-screen p-8 text-white">
+      <h1 className="text-3xl font-semibold">Business Page</h1>
+      <p>This page is loading correctly.</p>
+    </div>
+  );
+}
